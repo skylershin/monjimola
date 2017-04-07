3d6397d837d73797da40382a3fe9b8d45f307a9b
ohddii <ohddii@gmail.com>
2017-04-07 23:05:41 +0900

    1. asset image 추가 2. health, touchBackground 클래스추가

---
+++ Assets/Script/health.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class health : MonoBehaviour {
+	public int startingHealth = 1;
+	public int currentHealth;
+	public const int minHealth = 1;
+	
+	public void airCondition(int amount) {
+		currentHealth -= amount;
+
+		//health bar
+	}
+}
